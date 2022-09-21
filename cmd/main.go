1b09659b9757de725bff3420d8fad9ad11643996
Nikolai Voronin <Mitay89@bk.ru>
2022-09-21 23:21:44 +0300

    Update CLI handle

--- cmd/main.go
+++ cmd/main.go
@@ -16,14 +16,23 @@ func main() {
 	case "symbols":
 		fmt.Println("This is symbol list")
 	case "convert":
-		fmt.Println("This is symbol list")
+		tips := "convert -from USD -to RUB 100"
 		convertCmd := flag.NewFlagSet("convert", flag.ExitOnError)
-		fromVal := convertCmd.String("from", "USD", "convert -from USD -to RUB")
-		toVal := convertCmd.String("to", "USD", "convert -from USD -to RUB")
+		fromVal := convertCmd.String("from", "USD", tips)
+		toVal := convertCmd.String("to", "USD", tips)
 		convertCmd.Parse(os.Args[2:])
-		fmt.Println("From ", *fromVal, " to ", *toVal)
+		fmt.Println("From", *fromVal, "to", *toVal, convertCmd.Args()[0])
 	case "history":
-		fmt.Println("This is history")
+		tips := "history -from USD -to RUB -data_from 20220101 -data_to 20220201 100"
+		historyCmd := flag.NewFlagSet("history", flag.ExitOnError)
+		fromVal := historyCmd.String("from", "USD", tips)
+		toVal := historyCmd.String("to", "USD", tips)
+		fromDate := historyCmd.String("data_from", "", tips)
+		toDate := historyCmd.String("data_to", "", tips)
+		historyCmd.Parse(os.Args[2:])
+		fmt.Println("From", *fromVal, "to", *toVal, "date from", *fromDate, "date to", *toDate, historyCmd.Args()[0])
+	case "-h", "--help":
+		fmt.Println("help will be soon")
 	default:
 		fmt.Printf("unexpected subcommand: \"%v\". Use -h or --help for help\n", os.Args[1])
 		os.Exit(1)
